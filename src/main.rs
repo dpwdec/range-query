c7a81bdab28c7aacb7e1d4e1dd58885aa61a42b7
Dec Kolakowski <declankolakowski@gmail.com>
2022-05-15 14:37:37 +0100

    Add pre processing of collections

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,20 @@ trait Min<T> where T: PartialOrd + Bounded + Clone {
     fn x_min(&self) -> T;
 }
 
-impl<T> Min<T> for Vec<T> where T: PartialOrd + Bounded + Clone {
+impl<T> Min<T> for [T] where T: PartialOrd + Bounded + Clone {
     fn x_min(&self) -> T {
         self.iter().fold(T::max_value(), |acc, x| if x < &acc { x.clone() } else { acc })
     }
+}
+
+fn pre_process_collection<T>(x: Vec<T>) -> Vec<T> where T: PartialOrd + Bounded + Clone {
+    let chunk_size = (x.len() as f64).sqrt().ceil() as usize;
+    x
+        .chunks(chunk_size)
+        .fold(Vec::new(), |mut acc, chunk| {
+            acc.push(chunk.x_min());
+            acc
+        })
 }
 
 #[cfg(test)]
@@ -36,5 +46,11 @@ mod tests {
     fn test_float_collection() {
         let x = vec!(4.3, -2.9, 3.0, 9.0, -10.5, -10.6, 12.4, -1.0);
         assert_eq!(-10.6, x.x_min());
+    }
+
+    #[test]
+    fn test_pre_process_collection() {
+        let x = vec!(2, 3, 5, 7, 100, 0, 30, 21);
+        assert_eq!(vec!(2, 0, 21), pre_process_collection(x));
     }
 }
